8b3e0a40e11898008a29a4f8bb906a571bba8f52
Junyeol Lee <junyeol.ljay.lee@gmail.com>
2023-01-08 13:34:09 +0900

    Create 35. Search Insert Position.py

---
+++ leetcode/35. Search Insert Position.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/search-insert-position/description/
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+
+        left = 0
+        right = len(nums) - 1
+        
+        while left <= right:
+
+            mid = ( left + right ) // 2
+
+            print(left, right, mid)
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return left
