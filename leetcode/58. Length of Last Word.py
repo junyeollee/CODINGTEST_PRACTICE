60b26a68fda2646098d20f4de75af0274f6d9899
Junyeol Lee <junyeol.ljay.lee@gmail.com>
2023-01-10 22:40:50 +0900

    Create 58. Length of Last Word.py

---
+++ leetcode/58. Length of Last Word.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+
+        i = len(s) - 1
+        result = 0
+
+        while s[i] == ' ':
+            i -= 1
+        while i >= 0 and s[i] != ' ':
+            result += 1
+            i -= 1
+
+        return result
