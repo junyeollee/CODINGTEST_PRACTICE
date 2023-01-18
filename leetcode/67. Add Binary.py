f4833240bf89d09e6d25b7f1e9c774f7c5f323aa
Junyeol Lee <junyeol.ljay.lee@gmail.com>
2023-01-19 00:48:49 +0900

    Create 67. Add Binary.py

---
+++ leetcode/67. Add Binary.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/add-binary/description/
+
+class Solution:
+    def addBinary(self, a, b) -> str:
+        x, y = int(a, 2), int(b, 2)
+        while y:
+            answer = x ^ y
+            carry = (x & y) << 1
+            x, y = answer, carry
+        return bin(x)[2:]
